e423cf162319ec2d0ea8f76cb923b766ddbb63b4
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-07-17 17:07:53 -0400

    Change iframes to allowFullScreen

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -10,42 +10,42 @@ export default function Showcase() {
         >
           <iframe
             src="https://drive.google.com/file/d/1ZQFv9LXWPrSz8aS9pIox-pUq8d2Qsv7v/preview"
-            allow="autoplay"
+            allow="autoplay" allowFullScreen
           ></iframe>
         </PerformanceCard>
 
         <PerformanceCard id="card2" info="'Superstition' - Stevie Wonder">
           <iframe
             src="https://drive.google.com/file/d/1Gctqs9rCSIQ_zA8oN1UOLdjMsVKryuvr/preview"
-            allow="autoplay"
+            allow="autoplay" allowFullScreen
           ></iframe>
         </PerformanceCard>
 
         <PerformanceCard id="card3" info="'I Go to the Rock'">
           <iframe
             src="https://drive.google.com/file/d/1DayrliFM6KjIk5WYvtFiIA7rZRXOtF9_/preview"
-            allow="autoplay"
+            allow="autoplay" allowFullScreen
           ></iframe>
         </PerformanceCard>
 
         <PerformanceCard id="card4" info="'Up Above my Head'">
           <iframe
             src="https://drive.google.com/file/d/1NRykRq2LE2eZMJs_jsWo0VXixo9i-DpZ/preview"
-            allow="autoplay"
+            allow="autoplay" allowFullScreen
           ></iframe>
         </PerformanceCard>
 
         <PerformanceCard id="card5" info="'Higher' (Encore)">
           <iframe
             src="https://drive.google.com/file/d/1htdAaN32pBy_vKYPupYuZlzwvjI85pVC/preview"
-            allow="autoplay"
+            allow="autoplay" allowFullScreen
           ></iframe>
         </PerformanceCard>
 
         <PerformanceCard id="card6" info="'Overjoyed'">
           <iframe
             src="https://drive.google.com/file/d/1tuJ0zv5EDA3jJBmKeOFyeojq_1SN1zYZ/preview"
-            allow="autoplay"
+            allow="autoplay" allowFullScreen
           ></iframe>
         </PerformanceCard>
       </div>

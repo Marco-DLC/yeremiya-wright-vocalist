6fdd7e0bcc003457e3705d3a8a8ed4cabece230d
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-07-16 20:11:25 -0400

    Add responsiveness for mobile screens

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -3,36 +3,59 @@ export default function Showcase() {
     <div id="Showcase">
       <span id="showcaseScrl"></span>
       <h2>Performances</h2>
-      <PerformanceCard id='card1' info='&apos;Pulse of the Day&apos; - Alina Engibaryan'>
-      <iframe src="https://drive.google.com/file/d/1ZQFv9LXWPrSz8aS9pIox-pUq8d2Qsv7v/preview" allow="autoplay"></iframe>
+      <div className="showcase-grid">
+        <PerformanceCard
+          id="card1"
+          info="'Pulse of the Day' - Alina Engibaryan"
+        >
+          <iframe
+            src="https://drive.google.com/file/d/1ZQFv9LXWPrSz8aS9pIox-pUq8d2Qsv7v/preview"
+            allow="autoplay"
+          ></iframe>
         </PerformanceCard>
 
-      <PerformanceCard id='card2' info='&apos;Superstition&apos; - Stevie Wonder'>
-      <iframe src="https://drive.google.com/file/d/1Gctqs9rCSIQ_zA8oN1UOLdjMsVKryuvr/preview" allow="autoplay"></iframe>
+        <PerformanceCard id="card2" info="'Superstition' - Stevie Wonder">
+          <iframe
+            src="https://drive.google.com/file/d/1Gctqs9rCSIQ_zA8oN1UOLdjMsVKryuvr/preview"
+            allow="autoplay"
+          ></iframe>
         </PerformanceCard>
 
-      <PerformanceCard id='card3' info='&apos;I Go to the Rock&apos;'>
-      <iframe src="https://drive.google.com/file/d/1DayrliFM6KjIk5WYvtFiIA7rZRXOtF9_/preview" allow="autoplay"></iframe>
+        <PerformanceCard id="card3" info="'I Go to the Rock'">
+          <iframe
+            src="https://drive.google.com/file/d/1DayrliFM6KjIk5WYvtFiIA7rZRXOtF9_/preview"
+            allow="autoplay"
+          ></iframe>
         </PerformanceCard>
 
-      <PerformanceCard id='card4' info='&apos;Up Above my Head&apos;'>
-      <iframe src="https://drive.google.com/file/d/1NRykRq2LE2eZMJs_jsWo0VXixo9i-DpZ/preview"  allow="autoplay"></iframe>
+        <PerformanceCard id="card4" info="'Up Above my Head'">
+          <iframe
+            src="https://drive.google.com/file/d/1NRykRq2LE2eZMJs_jsWo0VXixo9i-DpZ/preview"
+            allow="autoplay"
+          ></iframe>
         </PerformanceCard>
 
-      <PerformanceCard id='card5' info='&apos;Higher&apos; (Encore)'>
-      <iframe src="https://drive.google.com/file/d/1htdAaN32pBy_vKYPupYuZlzwvjI85pVC/preview" allow="autoplay"></iframe>
+        <PerformanceCard id="card5" info="'Higher' (Encore)">
+          <iframe
+            src="https://drive.google.com/file/d/1htdAaN32pBy_vKYPupYuZlzwvjI85pVC/preview"
+            allow="autoplay"
+          ></iframe>
         </PerformanceCard>
 
-      <PerformanceCard id='card6' info='&apos;Overjoyed&apos;'>
-      <iframe src="https://drive.google.com/file/d/1tuJ0zv5EDA3jJBmKeOFyeojq_1SN1zYZ/preview"  allow="autoplay"></iframe>
+        <PerformanceCard id="card6" info="'Overjoyed'">
+          <iframe
+            src="https://drive.google.com/file/d/1tuJ0zv5EDA3jJBmKeOFyeojq_1SN1zYZ/preview"
+            allow="autoplay"
+          ></iframe>
         </PerformanceCard>
+      </div>
     </div>
   );
 }
 
-function PerformanceCard({id, children, info}) {
+function PerformanceCard({ id, children, info }) {
   return (
-    <div className='Card' id={id}>
+    <div className="Card" id={id}>
       <div className="video">{children}</div>
       <div className="info">
         <p>{info}</p>
